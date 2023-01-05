728e21364f73c248af817a55be65762dff9cac9d
August Aldred <hachigatsu14@outlook.com>
2023-01-05 15:07:51 +0000

    reinstate console logs

--- index.js
+++ index.js
@@ -7,18 +7,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/slack-webhook-verify', (req, res) => {
-  // console.log(req);
-  // console.log('Hello we are no logging\nfull body log:');
-  // console.log(req.body);
+  console.log('Hello we are no logging\nfull body log:');
+  console.log(req.body);
 
-  // console.log('log user profile:');
-  // console.log(req.body.event.user.profile);
+  console.log('log user profile:');
+  console.log(req.body.event.user.profile);
 
   // console.log('user email:', req.body.event.user.profile.email);
   // console.log('user slack id:', req.body.event.user.id);
 
   // required to return challenge for slack
-
   res.status(200).json({ challenge: req.body.challenge });
 });
 
